04f7081e94bd6ba061562259798145f031780a2c
JoelBluetab <115334335+JoelBluetab@users.noreply.github.com>
2022-10-08 09:26:56 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,8 +1,8 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
+        git('https://github.com/JoelBluetab/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('JoelGuty')
             node / gitConfigEmail('macloujulian@gmail.com')
         }
     }
