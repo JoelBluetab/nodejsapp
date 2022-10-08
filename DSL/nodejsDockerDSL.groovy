d89613fe6e69217cc9a79d25303a46a9fe52efe8
JoelBluetab <115334335+JoelBluetab@users.noreply.github.com>
2022-10-08 09:28:41 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('joelbluetab/nodejsapp')
+            repositoryName('joelbluetab/nodejsapp2')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
